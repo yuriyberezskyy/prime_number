05e219ce86b305a309f574f8f135e9c60ebeded5
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-17 17:59:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#Write your code below this line 👇
+def prime_checker(number):
+  is_prime = True
+  for i in range(2,number):
+    if number %  i == 0:
+      is_prime = False
+
+  if is_prime == True:
+    print("It is prime number")
+  else: 
+    print("It is not a prime number")
+  
+    
+    
+    
+    
+     
+
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
+
